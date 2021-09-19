1ba38d6263c99b3e49e188e02e06f0ccf6d2930d
gzx <1960602302@qq.com>
2021-09-19 18:50:56 +0800

    feat:2021-9-19 650. 只有两个键的键盘

---
+++ 2021-09/2021-9-19 650. 只有两个键的键盘/minSteps.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(minSteps(8))
+}
+
+func minSteps(n int) int {
+	if n == 1{
+		return 0
+	}
+	dp := make([]int,n+1)
+	for i:=2;i<=n;i++{
+		dp[i] = i
+
+		for j:=2;j<=getSqrt(n);j++{
+			if i % j == 0{
+				dp[i] = dp[j]+dp[i/j]
+				break
+			}
+		}
+	}
+	return dp[n]
+}
+
+func getSqrt(num int) int {
+	return int(math.Ceil(math.Sqrt(float64(num))))
+}
