c395b2e81689f9a8a7d381a2b40dd77423cc1b2e
gzx <1960602302@qq.com>
2021-09-21 18:16:29 +0800

    feat:2021-9-21每日一题提交

--- 2021-09/2021-9-21 58.最后一个单词的长度/lengthOfLastWord.go
+++ 2021-09/2021-9-21 58.最后一个单词的长度/lengthOfLastWord.go
@@ -1,5 +1,19 @@
 package main
 
 func main() {
+	s := "   fly me   to   the moon  "
+	lengthOfLastWord(s)
+}
 
+func lengthOfLastWord(s string) int {
+	l := len(s)-1
+	var res int
+	for i:= l;i>=0;i--{
+		if string(s[i]) != " "{
+			res ++
+		}else if res != 0{
+			return res
+		}
+	}
+	return res
 }
