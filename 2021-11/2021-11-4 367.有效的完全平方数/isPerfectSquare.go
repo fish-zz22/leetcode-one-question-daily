364ae4e4b26ca79f04285694ec7d815f91f8e5a0
gzx <1960602302@qq.com>
2021-11-04 09:23:10 +0800

    feat:打卡2021-11-4每日一题

--- 2021-11/2021-11-4 367.有效的完全平方数/isPerfectSquare.go
+++ 2021-11/2021-11-4 367.有效的完全平方数/isPerfectSquare.go
@@ -5,14 +5,11 @@ func main() {
 }
 
 func isPerfectSquare(num int) bool {
-	temp := 1
-	for temp <= num {
+	for temp := 1; temp <= num; temp++ {
 		if temp*temp > num {
 			return false
 		} else if temp*temp == num {
 			return true
-		} else {
-			temp++
 		}
 	}
 	return false
