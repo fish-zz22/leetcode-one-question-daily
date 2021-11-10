b130408a6af80782b45608322955cad36ad65b20
gzx <1960602302@qq.com>
2021-11-10 11:17:31 +0800

    feat:打卡2021-11-10每日一题

---
+++ 2021-11/2021-11-10 495.提莫攻击/findPoisonedDuration.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	timeSeries := []int{1, 2, 7}
+	duration := 3
+	fmt.Println(findPoisonedDuration(timeSeries, duration))
+}
+
+func findPoisonedDuration(timeSeries []int, duration int) int {
+	if len(timeSeries) == 1 {
+		return duration
+	}
+
+	var seconds int
+	for i := 1; i < len(timeSeries); i++ {
+		if timeSeries[i]-timeSeries[i-1] >= duration {
+			seconds += duration
+		} else {
+			seconds += timeSeries[i] - timeSeries[i-1]
+		}
+	}
+	seconds += duration
+	return seconds
+}
