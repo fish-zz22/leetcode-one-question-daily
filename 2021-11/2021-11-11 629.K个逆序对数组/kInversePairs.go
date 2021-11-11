daa9db625b6e0eaa6dc2eb62d7269480f1fe02e7
gzx <1960602302@qq.com>
2021-11-11 15:50:21 +0800

    feat:打卡2021-11-11每日一题(抄的题解)

--- 2021-11/2021-11-11 629.K个逆序对数组/kInversePairs.go
+++ 2021-11/2021-11-11 629.K个逆序对数组/kInversePairs.go
@@ -5,21 +5,20 @@ func main() {
 }
 
 func kInversePairs(n int, k int) int {
-	const mod = 1e9 + 7
-	dp := make([][]int, k+1)
-	dp[0] = make([]int, n+1)
+	var mod int64 = 1000000007
+	dp := make([][]int64, k+1)
+	dp[0] = make([]int64, n+1)
 	for i := 0; i <= n; i++ {
 		dp[0][i] = 1
 	}
 	for i := 1; i < len(dp); i++ {
-		dp[i] = make([]int, n+1)
+		dp[i] = make([]int64, n+1)
 		for j := 1; j <= n; j++ {
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod
 			if i >= j {
-				dp[i][j] = dp[i][j] - dp[i-1][j-1]
+				dp[i][j] = (dp[i][j] + mod - dp[i-j][j-1]) % mod
 			}
-			dp[i][j] = (dp[i][j] + mod) % mod
 		}
 	}
-	return dp[n][k]
+	return int(dp[k][n])
 }
