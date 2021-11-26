e10f9fc0f0e71409aa876ac8b63f40f5c4aa39e0
gzx <1960602302@qq.com>
2021-11-26 09:26:50 +0800

    feat:打卡2021-11-26每日一题

---
+++ 2021-11/2021-11-26 783.二叉搜索树中的搜索/searchBST.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+	return searchBST(root.Left, val)
+}
