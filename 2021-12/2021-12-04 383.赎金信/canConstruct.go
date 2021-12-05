23cedd736ca6b7a359428cc87673648161ff9cd1
gzx <1960602302@qq.com>
2021-12-05 21:13:45 +0800

    feat: 增加04解法

--- 2021-12/2021-12-04 383.赎金信/canConstruct.go
+++ 2021-12/2021-12-04 383.赎金信/canConstruct.go
@@ -5,16 +5,30 @@ func main() {
 }
 
 func canConstruct(ransomNote string, magazine string) bool {
-	hash := make(map[rune]int, len(magazine))
+	//hash := make(map[rune]int, len(magazine))
+	//for _, v := range magazine {
+	//	hash[v]++
+	//}
+	//
+	//for _, v := range ransomNote {
+	//	if hash[v] <= 0 {
+	//		return false
+	//	}
+	//	hash[v]--
+	//}
+	//return true
+
+	// 减少占用内存
+	var char [26]int
 	for _, v := range magazine {
-		hash[v]++
+		char[v-'a']++
 	}
 
 	for _, v := range ransomNote {
-		if hash[v] <= 0 {
+		if char[v-'a'] <= 0 {
 			return false
 		}
-		hash[v]--
+		char[v-'a']--
 	}
 	return true
 }
